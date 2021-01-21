7cdfc34dc03f9801ffceff9814866e49c474a8e4
DenysSh <Mufasishe@gmail.com>
2021-01-20 21:05:59 -0600

    Refactoring of doImportButton and customImportButton tests

--- src/test/java/EntityImportTest.java
+++ src/test/java/EntityImportTest.java
@@ -4,12 +4,15 @@ import model.RecycleBinPage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 import runner.ProjectUtils;
 
+import java.util.List;
 import java.util.UUID;
 
 public class EntityImportTest extends BaseTest {
@@ -31,99 +34,101 @@ public class EntityImportTest extends BaseTest {
         Assert.assertEquals(recycleBinPage.getDeletedImportValue(), str);
     }
 
-    @Ignore
     @Test
     public void doImportButton() throws InterruptedException{
 
-        WebDriver driver = getDriver();
-        ProjectUtils.loginProcedure(driver);
-
         final String str = "Denys_Test_1";
         final String text = "Do_Import_Button";
-        final int integer = 1;
-        final double decimal = 1.55;
+        final String integer = "1";
+        final String decimal = "1.55";
         final String user = "User 1 Demo";
 
+        WebDriver driver = getDriver();
+        WebDriverWait wait = new WebDriverWait(driver, 10);
         createRecordInImportValuesEntity(driver, str, text, integer, decimal);
 
         WebElement ImportEntity =
                 driver.findElement(By.xpath("//a[@href='index.php?action=action_list&entity_id=17']"));
         ProjectUtils.click(driver, ImportEntity);
+
         WebElement createImportFolder = driver.findElement(By.xpath("//i[text()='create_new_folder']"));
         createImportFolder.click();
+
         WebElement doImportButton = driver.findElement(By.xpath("//input[@value='Do import']"));
         doImportButton.click();
-        WebElement chooseRecord = driver.findElement(By.xpath("//i[text()='done_all']"));
-        chooseRecord.click();
-        Thread.sleep(1000);
-        WebElement saveButton = driver.findElement(By.xpath("//button[@id='pa-entity-form-save-btn']"));
-        ProjectUtils.click(driver, saveButton);
-        WebElement selectImportedRecord = driver.findElement(By.xpath("//button/i[@class='material-icons']"));
-        selectImportedRecord.click();
-        WebElement viewRecord = driver.findElement(By.xpath("//a[text()='view']"));
-        viewRecord.click();
 
-        final String FIELD_XPATH = "(//label[text()='String']/following::div/div/span)";
-        WebElement fieldString = driver.findElement(By.xpath(FIELD_XPATH + "[1]"));
-        WebElement fieldText = driver.findElement(By.xpath(FIELD_XPATH + "[2]"));
-        WebElement fieldInt = driver.findElement(By.xpath(FIELD_XPATH + "[3]"));
-        WebElement fieldDecimal = driver.findElement(By.xpath(FIELD_XPATH + "[4]"));
-        WebElement fieldUser = driver.findElement(By.xpath("//div[@class='form-group']//p"));
+        WebElement selectRecord = driver.findElement(By.xpath("//i[text()='done_all']"));
+        wait.until(ExpectedConditions.elementToBeClickable(selectRecord));
+        selectRecord.click();
+
+        WebElement saveButton = driver.findElement(By.xpath("//button[@id='pa-entity-form-save-btn']"));
+        ProjectUtils.scroll(driver, saveButton);
+        saveButton.click();
+
+        List<WebElement> importedRow = driver.findElements(By.xpath("//tbody/tr"));
+        Assert.assertEquals(importedRow.size(), 1);
+
+        WebElement fieldString = importedRow.get(0).findElement(By.xpath("//td[2]/a/div"));
+        WebElement fieldText = importedRow.get(0).findElement(By.xpath("//td[3]/a/div"));
+        WebElement fieldInt = importedRow.get(0).findElement(By.xpath("//td[4]/a/div"));
+        WebElement fieldDecimal = importedRow.get(0).findElement(By.xpath("//td[5]/a/div"));
+        WebElement fieldUser = importedRow.get(0).findElement(By.xpath("//td[9]"));
 
         Assert.assertEquals(fieldString.getText(), str);
         Assert.assertEquals(fieldText.getText(), text);
-        Assert.assertEquals(fieldInt.getText(), String.valueOf(integer));
-        Assert.assertEquals(fieldDecimal.getText(),String.valueOf(decimal));
+        Assert.assertEquals(fieldInt.getText(), integer);
+        Assert.assertEquals(fieldDecimal.getText(),decimal);
         Assert.assertEquals(fieldUser.getText(), user);
     }
 
-    @Ignore
     @Test
     public void customImportButton() throws InterruptedException{
 
-        WebDriver driver = getDriver();
-        ProjectUtils.loginProcedure(driver);
-
         final String str = "Denys_Test_2";
         final String text = "Custom_Import_Button";
-        final int integer = 2;
-        final double decimal = 2.55;
+        final String integer = "2";
+        final String decimal = "2.55";
         final String user = "User 1 Demo";
 
+        WebDriver driver = getDriver();
+        WebDriverWait wait = new WebDriverWait(driver, 10);
         createRecordInImportValuesEntity(driver, str, text, integer, decimal);
 
         WebElement ImportEntity =
                 driver.findElement(By.xpath("//a[@href='index.php?action=action_list&entity_id=17']"));
         ProjectUtils.click(driver, ImportEntity);
+
         WebElement createImportFolder = driver.findElement(By.xpath("//i[text()='create_new_folder']"));
         createImportFolder.click();
+
         WebElement doImportButton = driver.findElement(By.xpath("//input[@value='Custom Import']"));
         doImportButton.click();
-        WebElement chooseRecord = driver.findElement(By.xpath("//i[text()='done_all']"));
-        chooseRecord.click();
-        Thread.sleep(1000);
-        WebElement saveButton = driver.findElement(By.xpath("//button[@id='pa-entity-form-save-btn']"));
-        ProjectUtils.click(driver, saveButton);
-        WebElement selectImportedRecord = driver.findElement(By.xpath("//button/i[@class='material-icons']"));
-        selectImportedRecord.click();
-        WebElement viewRecord = driver.findElement(By.xpath("//a[text()='view']"));
-        viewRecord.click();
 
-        final String FIELD_XPATH = "(//label[text()='String']/following::div/div/span)";
-        WebElement fieldString = driver.findElement(By.xpath(FIELD_XPATH + "[1]"));
-        WebElement fieldText = driver.findElement(By.xpath(FIELD_XPATH + "[2]"));
-        WebElement fieldInt = driver.findElement(By.xpath(FIELD_XPATH + "[3]"));
-        WebElement fieldDecimal = driver.findElement(By.xpath(FIELD_XPATH + "[4]"));
-        WebElement fieldUser = driver.findElement(By.xpath("//div[@class='form-group']//p"));
+        WebElement selectRecord = driver.findElement(By.xpath("//i[text()='done_all']"));
+        wait.until(ExpectedConditions.elementToBeClickable(selectRecord));
+        selectRecord.click();
+
+        WebElement saveButton = driver.findElement(By.xpath("//button[@id='pa-entity-form-save-btn']"));
+        ProjectUtils.scroll(driver, saveButton);
+        saveButton.click();
+
+        List<WebElement> importedRow = driver.findElements(By.xpath("//tbody/tr"));
+        Assert.assertEquals(importedRow.size(), 1);
+
+        WebElement fieldString = importedRow.get(0).findElement(By.xpath("//td[2]/a/div"));
+        WebElement fieldText = importedRow.get(0).findElement(By.xpath("//td[3]/a/div"));
+        WebElement fieldInt = importedRow.get(0).findElement(By.xpath("//td[4]/a/div"));
+        WebElement fieldDecimal = importedRow.get(0).findElement(By.xpath("//td[5]/a/div"));
+        WebElement fieldUser = importedRow.get(0).findElement(By.xpath("//td[9]"));
 
         Assert.assertEquals(fieldString.getText(), "This is a custom TEXT");
         Assert.assertEquals(fieldText.getText(), text);
-        Assert.assertEquals(fieldInt.getText(), String.valueOf(integer));
-        Assert.assertEquals(fieldDecimal.getText(),String.valueOf(decimal));
+        Assert.assertEquals(fieldInt.getText(), integer);
+        Assert.assertEquals(fieldDecimal.getText(), decimal);
         Assert.assertEquals(fieldUser.getText(), user);
     }
 
-    public void createRecordInImportValuesEntity(WebDriver driver, String str, String text, int integ, double decimal){
+    public void createRecordInImportValuesEntity(WebDriver driver, String str, String text, String integ, String decimal){
 
         WebElement importValuesTab = driver.findElement(By.xpath("//p[contains(text(),'Import values')]"));
         ProjectUtils.click(driver, importValuesTab);
