d9395a23ffdb341aa9fea8ba82e6d098b7972fd4
Evgen R <64995034+disel20@users.noreply.github.com>
2021-01-20 23:06:22 -0500

    commit Test from Evgen Rio

--- src/test/java/runner/SimpleTest.java
+++ src/test/java/runner/SimpleTest.java
@@ -3,7 +3,6 @@ package runner;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import runner.BaseTest;
@@ -23,5 +22,33 @@ public class SimpleTest extends BaseTest {
         Thread.sleep(3000);
 
         //browser.close();
+    }
+
+    @Test
+    public void newTest() throws InterruptedException {
+
+        WebDriver driver = getDriver();
+        driver.get("https://github.com/SergeiDemyanenko/PlatformaticaQA");
+
+        WebElement button = driver.findElement(By.id("branch-select-menu"));
+        button.click();
+
+        Thread.sleep(2000);
+
+        WebElement link = driver.findElement(By.xpath("//footer/a[contains(text(), 'branches')]"));
+        link.click();
+        Assert.assertEquals(driver.getCurrentUrl(), "https://github.com/SergeiDemyanenko/PlatformaticaQA/branches");
+    }
+
+    @Test
+    public void evgenRioTestPage() throws InterruptedException {
+
+        WebDriver driver = getDriver();
+        driver.get("https://www.accuweather.com/en/us/brooklyn-ny/11210/weather-forecast/334651");
+
+        Thread.sleep(2000);
+
+        WebElement temperature = driver.findElement(By.xpath("//div[@class = 'temp']"));
+        Assert.assertTrue(temperature.isDisplayed());
     }
 }
