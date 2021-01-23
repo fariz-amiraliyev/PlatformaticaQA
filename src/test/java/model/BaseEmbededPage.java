9701ef7ecc52149715736437f545b22e2fdfa65f
ke-de-ve <kedeve@gmail.com>
2021-01-23 17:25:59 -0500

    BaseEmbededTablePage

---
+++ src/test/java/model/BaseEmbededPage.java
@@ -0,0 +1,69 @@
+package model;
+
+import com.beust.jcommander.Strings;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class BaseEmbededPage<TablePage> extends MainPage {
+
+    private static final String TBODY_XPASS = "//table[@class='pa-entity-table']/tbody";
+    private static final String ROW_NUM = "@data-row = '%d'";
+    private static final String XPASS_TDS = "//td";
+    private static final String XPASS_DELETE_X = "//td[@class='pa-row-delete-btn-col']/div/i";
+
+    @FindBy (xpath = TBODY_XPASS)
+    private WebElement body;
+
+    @FindBy(xpath = TBODY_XPASS + "/tr[starts-with(@id,'add-row-')]/td[@class='pa-add-row-btn-col']/button")
+    private WebElement buttonNewEmbeded;
+
+    @FindBy(xpath = TBODY_XPASS + "/tr[starts-with(@id,'row-') and @data-row > '0']")
+    private List<WebElement> trs;
+
+    protected List<WebElement> getRows() {
+        return trs;
+    }
+
+    public BaseEmbededPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public TablePage clickNewFolder() {
+        buttonNewEmbeded.click();
+        return (TablePage) this;
+    }
+
+    public int getRowCount() {
+        if (Strings.isStringEmpty(body.getText())) {
+            return 0;
+        } else {
+            return getRows().size();
+        }
+    }
+
+    public List<String> getRow(int rowNumber) {
+        List<String> result = new ArrayList<>();
+        List<WebElement> cells = getRows().get(rowNumber).findElements(By.xpath(XPASS_TDS));
+
+        for (int i = 1; i < cells.size()-1; i++ ) {
+            result.add(cells.get(i).getText());
+        }
+
+        return result;
+    }
+
+    public TablePage deleteRow(int rowNumber) {
+        trs.get(rowNumber).findElement(By.xpath(XPASS_DELETE_X)).click();
+        return (TablePage) this;
+    }
+
+    public TablePage deleteRow() {
+        return deleteRow(getRows().size() - 1);
+    }
+
+}
