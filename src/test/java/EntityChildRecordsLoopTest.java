7d83ca51d53aee8e7daa12358eedd398439208f2
GalinaRuban <galina.ruban11@gmail.com>
2021-01-24 21:37:49 -0500

    As requested, I moved all xpaths (except for List) to the start of my class as By fields.

--- src/test/java/EntityChildRecordsLoopTest.java
+++ src/test/java/EntityChildRecordsLoopTest.java
@@ -14,6 +14,19 @@ import java.util.Random;
 @Run(run = RunType.Multiple)
 public class EntityChildRecordsLoopTest extends BaseTest {
 
+    private static final By BY_CREATE_NEW = By.xpath("//i[contains(text(), 'create_new_folder')]/ancestor::a");
+    private static final By CHILD_RECORD_LOOP = By.xpath("//p[contains (text(), 'Child records loop')]/parent::a");
+    private static final By CHILD_RECORD = By.xpath("//p[contains(text(), 'Child records loop')]/ancestor::a");
+    private static final By RECORD_MENU = By.xpath("//button[@class='btn btn-round btn-sm btn-primary dropdown-toggle']");
+    private static final By SAVE_BTN = By.xpath("//button[@id='pa-entity-form-save-btn']");
+    private static final By END_BALANCE = By.xpath("//input[@id='end_balance']");
+    private static final By LAST_LINE = By.xpath("//tr//textarea[@id='t-68-r-9-amount']");
+    private static final By VIEW_FUNCTION = By.xpath("//a[text() = 'view']");
+    private static final By EDIT_FUNCTION = By.xpath("//a[text() = 'edit']");
+    private static final By START_BALANCE_FIELD = By.xpath("//input[@id='start_balance']");
+    private static final By END_BALANCE_FIELD = By.xpath("//input[@id='end_balance']");
+    private static final By GREEN_PLUS = By.xpath("//button[@data-table_id='68']");
+
     private static final String START_BALANCE = "1";
     private static final String VALUE_9 = "6";
     private static int endBalanceD;
@@ -23,10 +36,8 @@ public class EntityChildRecordsLoopTest extends BaseTest {
     private static final double[] FIRST_VALUES_PASSED = {0.00, 10.50, 11.00, 12.00, 13.00, 14.00, 1.00, 1.00, 2.50, 0.0};
 
     private void addingRowsByClickingOnGreenPlus(int n) {
-        WebDriver driver = getDriver();
-        WebElement greenPlus = driver.findElement(By.xpath("//button[@data-table_id='68']"));
         for (int i = 1; i <= n; i++) {
-            ProjectUtils.click(driver, greenPlus);
+            ProjectUtils.click(getDriver(), getDriver().findElement(GREEN_PLUS));
         }
     }
 
@@ -35,17 +46,13 @@ public class EntityChildRecordsLoopTest extends BaseTest {
     }
 
     private void createNewChildLoopEmptyRecord() {
-        WebDriver driver = getDriver();
-        WebElement childRecordsLoop = driver.findElement(By.xpath("//p[contains (text(), 'Child records loop')]/parent::a"));
-        ProjectUtils.click(driver, childRecordsLoop);
-        WebElement createNew = driver.findElement(By.xpath("//i[contains(text(), 'create_new_folder')]/ancestor::a"));
-        ProjectUtils.click(driver, createNew);
+        ProjectUtils.click(getDriver(), getDriver().findElement(CHILD_RECORD_LOOP));
+        ProjectUtils.click(getDriver(), getDriver().findElement(BY_CREATE_NEW));
         addingRowsByClickingOnGreenPlus(NUMBERS_OF_LINES);
     }
 
     private void fillData(String xpath, String valueSend) {
-        WebDriver driver = getDriver();
-        WebElement line = driver.findElement(By.xpath(xpath));
+        WebElement line = getDriver().findElement(By.xpath(xpath));
         line.clear();
         line.sendKeys(valueSend);
         getWebDriverWait().until(d -> !Strings.isStringEmpty(line.getAttribute("value"))
@@ -53,31 +60,24 @@ public class EntityChildRecordsLoopTest extends BaseTest {
     }
 
     private void deleteRows(int rowNumber) {
-        WebDriver driver = getDriver();
-        WebElement deleteLine = driver.findElement(By.xpath(String.format("//i[@data-row= '%d'  and contains(text(), 'clear')]", rowNumber)));
-        ProjectUtils.click(driver, deleteLine);
+        WebElement deleteLine = getDriver().findElement(By.xpath(String.format("//i[@data-row= '%d'  and contains(text(), 'clear')]", rowNumber)));
+        ProjectUtils.click(getDriver(), deleteLine);
         getWebDriverWait().until(d -> !deleteLine.isDisplayed());
     }
 
-   private void goToChildLoop() {
-        WebDriver driver = getDriver();
-        WebElement childRecord = driver.findElement(By.xpath("//p[contains(text(), 'Child records loop')]/ancestor::a"));
-        ProjectUtils.click(driver, childRecord);
-        WebElement recordMenu = driver.findElement(By.xpath("//button[@class='btn btn-round btn-sm btn-primary dropdown-toggle']"));
-        ProjectUtils.click(driver, recordMenu);
+    private void goToChildLoop() {
+        ProjectUtils.click(getDriver(), getDriver().findElement(CHILD_RECORD));
+        ProjectUtils.click(getDriver(), getDriver().findElement(RECORD_MENU));
     }
 
     @Test
     public void checkStartEndBalanceBeforeSave() {
 
-        WebDriver driver = getDriver();
         createNewChildLoopEmptyRecord();
 
-        driver.findElement(By.xpath("//input[@id='start_balance']")).sendKeys(START_BALANCE);
+        getDriver().findElement(START_BALANCE_FIELD).sendKeys(START_BALANCE);
 
-        WebElement endBalance = driver.findElement(By.xpath("//input[@id='end_balance']"));
-
-        getWebDriverWait().until(d -> endBalance.getAttribute("value").equals(START_BALANCE));
+        getWebDriverWait().until(d -> getDriver().findElement(END_BALANCE).getAttribute("value").equals(START_BALANCE));
 
         sumNumber += Integer.parseInt(START_BALANCE);
 
@@ -89,50 +89,45 @@ public class EntityChildRecordsLoopTest extends BaseTest {
             fillData(String.format("//tr//textarea[@id='t-68-r-%d-amount']", i), String.valueOf(FIRST_VALUES_PASSED[i]));
         }
 
-        WebElement lastLine = driver.findElement(By.xpath("//tr//textarea[@id='t-68-r-9-amount']"));
+        getWebDriverWait().until(d -> getDriver().findElement(LAST_LINE).getAttribute("value").equals(String.valueOf(FIRST_VALUES_PASSED[9])));
 
-        getWebDriverWait().until(d -> lastLine.getAttribute("value").equals(String.valueOf(FIRST_VALUES_PASSED[9])));
-
-        List<WebElement> tableLines = driver.findElements(By.xpath("//textarea[@class='pa-entity-table-textarea pa-table-field t-68-amount']"));
+        List<WebElement> tableLines = getDriver().findElements(By.xpath("//textarea[@class='pa-entity-table-textarea pa-table-field t-68-amount']"));
         Assert.assertEquals(tableLines.size(), FIRST_VALUES_PASSED.length - 1);
 
-        getWebDriverWait().until(d -> endBalance.getAttribute("value").equals(String.valueOf((int)sumNumber)));
+        getWebDriverWait().until(d -> getDriver().findElement(END_BALANCE).getAttribute("value").equals(String.valueOf((int) sumNumber)));
 
         deleteRows(4);
         deleteRows(6);
 
         final double sum = sumNumber - FIRST_VALUES_PASSED[4] - FIRST_VALUES_PASSED[6];
-        getWebDriverWait().until(d -> endBalance.getAttribute("value").equals(String.valueOf((int)(sum))));
+        getWebDriverWait().until(d -> getDriver().findElement(END_BALANCE).getAttribute("value").equals(String.valueOf((int) (sum))));
 
         addingRowsByClickingOnGreenPlus(randomIntGeneration(1, 5));
 
         final double endBalanceDigit = sum + Integer.parseInt(VALUE_9);
-        endBalanceD =  (int) endBalanceDigit;
+        endBalanceD = (int) endBalanceDigit;
 
         tableLines.get(FIRST_VALUES_PASSED.length - 2).clear();
         tableLines.get(FIRST_VALUES_PASSED.length - 2).sendKeys(VALUE_9);
 
-        getWebDriverWait().until(d -> (endBalance.getAttribute("value").equals(String.valueOf( (int) endBalanceDigit))));
+        getWebDriverWait().until(d -> (getDriver().findElement(END_BALANCE).getAttribute("value").equals(String.valueOf((int) endBalanceDigit))));
 
-        WebElement saveBtn = driver.findElement(By.xpath("//button[@id='pa-entity-form-save-btn']"));
-        ProjectUtils.click(driver, saveBtn);
+        ProjectUtils.click(getDriver(), getDriver().findElement(SAVE_BTN));
 
         String partOfXpath = Integer.toString((int) endBalanceDigit);
 
-        getWebDriverWait().until(d -> driver.findElement(By.xpath("//div[contains(text(),'" + partOfXpath + "')]")).isDisplayed());
+        getWebDriverWait().until(d -> getDriver().findElement(By.xpath("//div[contains(text(),'" + partOfXpath + "')]")).isDisplayed());
     }
 
     @Test(dependsOnMethods = "checkStartEndBalanceBeforeSave")
     public void checkStartEndBalanceInViewMode() {
 
-        WebDriver driver = getDriver();
         goToChildLoop();
-        WebElement viewFunction = driver.findElement(By.xpath("//a[text() = 'view']"));
-        ProjectUtils.click(driver, viewFunction);
+        ProjectUtils.click(getDriver(), getDriver().findElement(VIEW_FUNCTION));
 
         String[] valuesArr = {START_BALANCE + ".00", endBalanceD + ".00"};
 
-        List<WebElement> startEndBalance = driver.findElements(By.xpath("//div/span[@class='pa-view-field']"));
+        List<WebElement> startEndBalance = getDriver().findElements(By.xpath("//div/span[@class='pa-view-field']"));
         for (int i = 0; i < startEndBalance.size(); i++) {
             Assert.assertEquals(startEndBalance.get(i).getText(), valuesArr[i]);
         }
@@ -140,14 +135,10 @@ public class EntityChildRecordsLoopTest extends BaseTest {
 
     @Test(dependsOnMethods = "checkStartEndBalanceBeforeSave")
     public void checkStartEndBalanceInEditMode() {
-        WebDriver driver = getDriver();
-        goToChildLoop();
-        WebElement editFunction = driver.findElement(By.xpath("//a[text() = 'edit']"));
-        ProjectUtils.click(driver, editFunction);
 
-        WebElement startBalanceField = driver.findElement(By.xpath("//input[@id='start_balance']"));
-        WebElement endBalance = driver.findElement(By.xpath("//input[@id='end_balance']"));
-        Assert.assertEquals(startBalanceField.getAttribute("value"), START_BALANCE);
-        Assert.assertEquals(endBalance.getAttribute("value"), String.valueOf(endBalanceD));
+        goToChildLoop();
+        ProjectUtils.click(getDriver(), getDriver().findElement(EDIT_FUNCTION));
+        Assert.assertEquals(getDriver().findElement(START_BALANCE_FIELD).getAttribute("value"), START_BALANCE);
+        Assert.assertEquals(getDriver().findElement(END_BALANCE_FIELD).getAttribute("value"), String.valueOf(endBalanceD));
     }
 }
